52fefe1333fa852398b89940f6201e4640f81dfa
Jerry Ying <90186306+JERRYYING02@users.noreply.github.com>
2023-10-15 05:45:09 +0100

    hi

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/deploying-vite-project/",
+  base: "/portfolio-jerry/",
   build: { chunkSizeWarningLimit: 1600, }
 })
